ca2201d92ecc6a2de897f6e68ee69e30cb75c11a
unnawut <unnawut@unnawut.com>
2025-03-17 17:33:27 +0700

    fix: less padding on mobile

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Dashboard } from "@/components/Dashboard"
 
 export default function Home() {
   return (
-    <main className="min-h-screen p-6">
+    <main className="min-h-screen p-0 py-6 md:p-6">
       <Dashboard />
     </main>
   )
